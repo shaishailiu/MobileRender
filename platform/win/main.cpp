f7b9daf97bf6aca6adef1f9440c33d6fce7442bf
shaishailiu <shaishailiu@163.com>
2020-01-18 23:04:41 +0800

    change window title

--- platform/win/main.cpp
+++ platform/win/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char * argv[]) {
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* mWindow = glfwCreateWindow(mWidth, mHeight, "GL4Cookbook", nullptr, nullptr);
+    GLFWwindow* mWindow = glfwCreateWindow(mWidth, mHeight, "Render", nullptr, nullptr);
 
     if (mWindow == nullptr) {
         std::cout << "Failed to create GLFW window" << std::endl;
